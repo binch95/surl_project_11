24e6fb93cd662028cd5ec32a95b159cb3aedd7f7
binch95 <qlscm95@gmail.com>
2024-11-27 15:40:47 +0900

    apiíkey 도입 및 username 대ì신 id활용

--- src/main/java/com/koreait/surl_project_11/global/security/SecurityConfig.java
+++ src/main/java/com/koreait/surl_project_11/global/security/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.koreait.surl_project_11.global.security;
 
+import com.koreait.surl_project_11.global.rsData.RsData;
+import com.koreait.surl_project_11.standard.util.Ut;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -20,6 +23,7 @@ public class SecurityConfig {
         http
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests
+                                .requestMatchers(HttpMethod.POST, "/api/*/members", "/api/*/members/login").permitAll()
                                 .requestMatchers("/h2-console/**").permitAll()
                                 .requestMatchers("/actuator/**").permitAll()
                                 .anyRequest().authenticated()
@@ -37,7 +41,22 @@ public class SecurityConfig {
                 ) // 타임리프, MPA에서는 csrf를 사용함 // REST API 방식에서는 끈다
                 .formLogin(formLogin ->
                         formLogin.permitAll()
-                ).addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                )
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(
+                                (request, response, authException) -> {
+                                    response.setContentType("application/json; charset=utf-8");
+                                    response.setStatus(403);
+                                    response.getWriter().write(
+                                            Ut.json.toString(
+                                                    RsData.of("403-1", request.getRequestURI() + ", " + authException.getLocalizedMessage())
+                                            )
+                                    );
+                                }
+                        ))
+
+
+                .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
